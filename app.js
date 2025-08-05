3ccf8ef97cd7dc294a6c678e784f590e891c3416
MuhammadMobeenArif <mobeenarif13@gmail.com>
2025-08-05 22:55:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const products = [
     },
     {
         id: 6,
-        image: "./images/products/BBQ chicken pizza.webp",
+        image: "./images/products/BBQ-chicken-pizza.webp",
         title: "BBQ Chicken Pizza",
         description: "BBQ chicken, cheese, and tangy sauce on a crispy base.",
         price: 700,
